060e30ef5d73c66a8665d0492ce5af7f6f5a2a54
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-05 11:39:35 +0300

    modified index.js to pass all the tests, completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+
+
+const currentUser = "Grace"; 
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+
+
+
+
+
